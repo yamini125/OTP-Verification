6bd525be2400ce62c5cba0a24bb2d8c6ffa5a5d0
yamini125 <55278030+yamini125@users.noreply.github.com>
2020-08-05 15:45:02 +0530

    Update otp.py
    changed the authentication code

--- otp.py
+++ otp.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 otp=random.randint(10000,100000)
 
 account_sid='ACf316d0803364c78eff7c5a01e198d0da'
-auth_token='b9872cb4310a9f7b1b960f1759b02e02'
+auth_token='c1c4f84b798da46647f85c88f051ee88'
 client=Client(account_sid,auth_token)
 client.messages.create(from_='+12057934385',to='+918825859851',body=str(otp))
 aaa=int(input())
